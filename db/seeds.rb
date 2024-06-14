ff808353047a565d11b894d97211b3b8bf9996cc
bradzara <bradzara93@gmail.com>
2024-06-14 09:37:07 -0700

    uncomment seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,10 +15,10 @@ categories = ['Art', 'Comics', 'Crafts', 'Dance', 'Design', 'Fashion', 'Film', '
 
 ## populate categories (MODEL SHOULD NOT ALLOW DUPLCATES) 
 ## NEED THIS TO POPULATE PROJECTS 
-# categories.each do |c|
-#   cat = Category.new(name: c)
-#   cat.save
-# end
+categories.each do |c|
+  cat = Category.new(name: c)
+  cat.save
+end
 
 
 project_types = ['Project', 'Mission', 'Foundation', 'Initiative', 'Kickstarter']
@@ -26,24 +26,24 @@ project_goals = ['Improvement', 'Development', 'Accessibility', 'Preservation', 
 
 
 ### POPULATES THE PROJECTS DATABASE IF UNCOMMENTED. rails db:seed
-# 10.times do
-#   str_date = Faker::Date.between(from: '2023-01-01', to: '2026-12-31')
-#   end_date = str_date + rand(15..450)
+10.times do
+  str_date = Faker::Date.between(from: '2023-01-01', to: '2026-12-31')
+  end_date = str_date + rand(15..450)
 
-#   goal_amount = rand(600..5000000)
+  goal_amount = rand(600..5000000)
 
-#   category = categories[rand(0...categories.length)]
-#   category_id = Category.find_by(name: category).id
+  category = categories[rand(0...categories.length)]
+  category_id = Category.find_by(name: category).id
 
-#   project = project_types[rand(0...project_types.length)]
-#   project_goal = project_goals[rand(0...project_goals.length)]
-#   maker = Faker::Name.name
+  project = project_types[rand(0...project_types.length)]
+  project_goal = project_goals[rand(0...project_goals.length)]
+  maker = Faker::Name.name
 
 
-#   title = "#{maker}'s #{project} for the #{project_goal} of #{category}"
-#   description = "#{maker} would like to thank everyone for their generous support!"
+  title = "#{maker}'s #{project} for the #{project_goal} of #{category}"
+  description = "#{maker} would like to thank everyone for their generous support!"
 
-#   proj = Project.new(category_id: category_id, title: title, description: description, goal_amount: goal_amount, start_date: str_date, end_date: end_date)
-#   proj.save
+  proj = Project.new(category_id: category_id, title: title, description: description, goal_amount: goal_amount, start_date: str_date, end_date: end_date)
+  proj.save
 
-# end
+end
