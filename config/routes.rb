551952c0aace571fe760ab6df8931a7fdc3cee36
andrew-wulf <andrewdwulf@gmail.com>
2024-06-14 18:47:00 -0500

    keeping my added route

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
 
   # User Routes
   post "/users" => "users#create"
+  get "/users/current" => "users#current"
 
   #FOR TESTING (should require admin permissions later)
   get "/users" => "users#index"
